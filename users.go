2b593df1c1f498afa0738ea811c61026ac499390
Daniel Hejduk <danielhejduk@disroot.org>
2025-01-08 19:09:59 +0100

    Adding a structure to the users file

--- users.go
+++ users.go
@@ -1,5 +1,31 @@
 package main
 
-func (server *server) add_user(id int) {} // TODO
+import (
+    "database/sql"
 
-func (server *server) remove_user(id int) {} // TODO
+    _ "github.com/lib/pq"
+)
+
+func (server *server) add_user(id int, name string) {
+    db, err := sql.Open("postgres", server.db_string)
+
+    if err != nil {}
+    
+    // INSERT INTO players (id, name) VALUES (1, 'John Doe');
+
+    _, err = db.Exec("INSERT INTO players (id, name) VALUES ($1, '$2');", id, name)
+
+    if err != nil {}
+
+    db.Close()
+
+}   // TODO
+
+func (server *server) remove_user(id int) {
+    db, err := sql.Open("postgres", server.db_string)
+
+    if err != nil {}
+
+    db.Close()
+
+} // TODO
