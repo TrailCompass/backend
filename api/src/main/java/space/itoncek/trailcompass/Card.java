02642fb4a74ec7a8eb436985f42c95365f807f91
Made By IToncek <itoncek3@itoncek.space>
2025-01-23 22:05:35 +0100

    CI 🤞

--- api/src/main/java/space/itoncek/trailcompass/Card.java
+++ api/src/main/java/space/itoncek/trailcompass/Card.java
@@ -1,6 +1,16 @@
 package space.itoncek.trailcompass;
 
+/**
+ * Represents a card, that can be played during a game.
+ */
 public interface Card {
+	/**
+	 * @return Name of the card
+	 */
 	String getName();
+
+	/**
+	 * @return Description of the card
+	 */
 	String getDescription();
 }
--- api/src/main/java/space/itoncek/trailcompass/Package.java
+++ api/src/main/java/space/itoncek/trailcompass/Package.java
@@ -4,9 +4,33 @@ import org.slf4j.Logger;
 
 import java.util.List;
 
+/**
+ * Main class, representing a {@link Package}, that is to be loaded by the server.
+ */
 public interface Package {
+	/**
+	 * This method acts as the constructor, gets called with necessary objects for optimal experience.
+	 * <p><b>NO LOGIC SHOULD BE DONE IN HERE! THE SERVER IS PROBABLY NOT READY!</b>
+	 * <p>This method should only save the input parameters into variables and return without doing any logic.
+	 * @param logger SLF4J-class logger, supplied by the server
+	 */
 	void onLoad(Logger logger);
+
+	/**
+	 * Gets called on server startup. Server is ready to interact with clients, preliminary setup shall be done in this method.
+	 */
 	void onEnable();
-	void onDisable();
+
+	/**
+	 * This method defines the cards, this plugin supplied
+	 * @hidden  Needs to be decided, whether this method should be called only once or any time the server is requesting cards.
+	 * @return List of cards to be registered in the game.
+	 */
 	List<Card> getCards();
+
+	/**
+	 * Gets called on server shutdown. Any open connection shall be closed here. After this method returns, this package
+	 * is considered dead.
+	 */
+	void onDisable();
 }
