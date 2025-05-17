957feeae43c833a0e8325884c0c7d07b24237c73
Made By IToncek <itoncek3@itoncek.space>
2025-05-17 20:51:10 +0200

    Mapserver init

--- server/src/main/java/space/itoncek/trailcompass/exchange/Exchange.java
+++ server/src/main/java/space/itoncek/trailcompass/exchange/Exchange.java
@@ -2,16 +2,19 @@ package space.itoncek.trailcompass.exchange;
 
 import space.itoncek.trailcompass.TrailServer;
 import space.itoncek.trailcompass.commons.exchange.IExchange;
+import space.itoncek.trailcompass.commons.exchange.IMapExchange;
 
 public class Exchange implements IExchange {
 	private final TrailServer server;
 	private final AuthExchange ax;
 	private final SystemExchange sx;
+	private MapExchange mx;
 
 	public Exchange(TrailServer server) {
 		this.server = server;
 		ax = new AuthExchange(server);
 		sx = new SystemExchange(server);
+		mx = new MapExchange(server);
 	}
 
 	@Override
@@ -22,5 +25,10 @@ public class Exchange implements IExchange {
 	@Override
 	public SystemExchange system() {
 		return sx;
+	}
+
+	@Override
+	public IMapExchange map() {
+		return mx;
 	}
 }
---
+++ server/src/main/java/space/itoncek/trailcompass/exchange/MapExchange.java
@@ -0,0 +1,72 @@
+package space.itoncek.trailcompass.exchange;
+
+import com.geodesk.feature.FeatureLibrary;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import space.itoncek.trailcompass.TrailServer;
+import space.itoncek.trailcompass.commons.exchange.IMapExchange;
+import space.itoncek.trailcompass.commons.requests.map.MapHashRequest;
+import space.itoncek.trailcompass.commons.requests.map.MapRequest;
+import space.itoncek.trailcompass.commons.responses.map.MapHashResponse;
+import space.itoncek.trailcompass.commons.responses.map.MapResponse;
+import space.itoncek.trailcompass.commons.utils.BackendException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.time.ZonedDateTime;
+
+public class MapExchange implements IMapExchange {
+	private final TrailServer server;
+	private final JSONObject config;
+	private final FeatureLibrary flib;
+	private String sha256 = null;
+	private ZonedDateTime timeout = ZonedDateTime.now().minusYears(100);
+
+	public MapExchange(TrailServer server) {
+		this.server = server;
+		new File("./data").mkdirs();
+		try {
+			if (!new File("./data/mapconfig.json").exists()) {
+				Files.writeString(new File("./data/mapconfig.json").toPath(),
+						new JSONObject()
+								.put("feature-library", "flib.gol")
+								.put("android-map", "prague.osm.map")
+								.toString(4),
+						StandardOpenOption.CREATE);
+			}
+			this.config = new JSONObject(Files.readString(new File("./data/mapconfig.json").toPath()));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		flib = new FeatureLibrary("./data/" + config.getString("feature-library"));
+	}
+
+	@Override
+	public MapHashResponse getMapHash(MapHashRequest request) throws BackendException {
+		try {
+			if (sha256 == null || ZonedDateTime.now().isAfter(timeout)) {
+				sha256 = DigestUtils.sha256Hex(new FileInputStream("./data/" + config.getString("android-map")));
+				timeout = ZonedDateTime.now().plusMinutes(10);
+			}
+			return new MapHashResponse(sha256);
+		} catch (Exception e) {
+			throw new BackendException(e);
+		}
+	}
+
+	@Override
+	public MapResponse getMap(MapRequest request) throws BackendException {
+		try (FileInputStream fis = new FileInputStream("./data/" + config.getString("android-map"))){
+			byte[] bytes = IOUtils.toByteArray(fis);
+			return new MapResponse(bytes);
+		} catch (Exception e) {
+			throw new BackendException(e);
+		} finally {
+			System.gc();
+		}
+	}
+}
