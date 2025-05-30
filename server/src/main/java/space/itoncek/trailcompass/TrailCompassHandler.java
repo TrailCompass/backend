f7ddf72af353de8a270239756ab4678c9eb8d576
Made By IToncek <itoncek3@itoncek.space>
2025-05-30 18:05:29 +0200

    Linking unlinked requests

--- server/src/main/java/space/itoncek/trailcompass/TrailCompassHandler.java
+++ server/src/main/java/space/itoncek/trailcompass/TrailCompassHandler.java
@@ -4,10 +4,7 @@ import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-import space.itoncek.trailcompass.commons.requests.auth.LoginRequest;
-import space.itoncek.trailcompass.commons.requests.auth.ProfileOtherRequest;
-import space.itoncek.trailcompass.commons.requests.auth.ProfileRequest;
-import space.itoncek.trailcompass.commons.requests.auth.RegisterRequest;
+import space.itoncek.trailcompass.commons.requests.auth.*;
 import space.itoncek.trailcompass.commons.requests.gamemgr.*;
 import space.itoncek.trailcompass.commons.requests.map.MapHashRequest;
 import space.itoncek.trailcompass.commons.requests.map.MapRequest;
@@ -49,12 +46,15 @@ public class TrailCompassHandler {
 				case RegisterRequest req -> ex.auth().register(req);
 				case ProfileRequest req -> ex.auth().getProfile(req);
 				case ProfileOtherRequest req -> ex.auth().getOtherProfile(req);
+				case ListPlayersRequest req -> ex.auth().listPlayers(req);
 				/* Map */
 				case MapHashRequest req -> ex.map().getMapHash(req);
 				case MapRequest req -> ex.map().getMap(req);
 				/* GameManager */
-				case GameStateRequest req -> ex.gameMgr().getGameState(req);
 				case CurrentHiderRequest req -> ex.gameMgr().getCurrentHider(req);
+				case EndingTimeRequest req -> ex.gameMgr().getEndingTime(req);
+				case GameStateRequest req -> ex.gameMgr().getGameState(req);
+				case SeekerLocationRequest req -> ex.gameMgr().getSeekerLocation(req);
 				case StartingTimeRequest req -> ex.gameMgr().getStartingTime(req);
 				/* default */
 				case null, default -> new ErrorResponse("There is no handler for that request!");
--- server/src/main/java/space/itoncek/trailcompass/exchange/GameManagerExchange.java
+++ server/src/main/java/space/itoncek/trailcompass/exchange/GameManagerExchange.java
@@ -8,16 +8,13 @@ import space.itoncek.trailcompass.commons.requests.gamemgr.*;
 import space.itoncek.trailcompass.commons.responses.gamemgr.CurrentHiderResponse;
 import space.itoncek.trailcompass.commons.responses.gamemgr.GameStateResponse;
 import space.itoncek.trailcompass.commons.responses.gamemgr.SeekerLocationResponse;
-import space.itoncek.trailcompass.commons.responses.gamemgr.StartingTimeResponse;
-import space.itoncek.trailcompass.commons.responses.generic.OkResponse;
+import space.itoncek.trailcompass.commons.responses.gamemgr.TimeResponse;
 import space.itoncek.trailcompass.commons.utils.BackendException;
 import space.itoncek.trailcompass.database.DatabasePlayer;
 import space.itoncek.trailcompass.database.LocationEntry;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class GameManagerExchange implements IGameManagerExchange {
 	private final TrailServer server;
@@ -55,18 +52,32 @@ public class GameManagerExchange implements IGameManagerExchange {
     }
 
 	@Override
-	public StartingTimeResponse getStartingTime(StartingTimeRequest request) throws BackendException {
+	public TimeResponse getStartingTime(StartingTimeRequest request) throws BackendException {
 		DecodedJWT jwt = server.tch.ex.auth().getJWTToken(request);
 		if (jwt == null) {
 			return null;
 		}
 
         try {
-            return new StartingTimeResponse(server.gm.getStartingTime());
+            return new TimeResponse(server.config.getConfig().getTime().getStartTime());
         } catch (IOException e) {
             throw new BackendException(e);
         }
     }
+
+	@Override
+	public TimeResponse getEndingTime(EndingTimeRequest request) throws BackendException {
+		DecodedJWT jwt = server.tch.ex.auth().getJWTToken(request);
+		if (jwt == null) {
+			return null;
+		}
+
+		try {
+			return new TimeResponse(server.config.getConfig().getTime().getEndTime());
+		} catch (IOException e) {
+			throw new BackendException(e);
+		}
+	}
 
 	@Override
 	public SeekerLocationResponse getSeekerLocation(SeekerLocationRequest request) throws BackendException {
